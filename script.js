54379024019725b33c10300f8343bb18fc9b8c1a
Leticia Coto <lcoto05@yahoo.com>
2025-10-08 14:48:46 -0400

    Added javascript so that buttons are used to start the rounds and score is also tracked and shown to the user, with logic to end the game. Play again logic was added. CSS stylings were added.

--- script.js
+++ script.js
@@ -15,45 +15,121 @@ function getComputerChoice() {
     
 }
 
-function getHumanChoice() {
-    choice = prompt('Rock, paper, or scissors?')
-    return choice.toLowerCase();
+const bodyref = document.querySelector("body")
+
+const score_button_container = document.createElement("div")
+
+
+let scoreDiv = document.createElement("div");
+let computerResults = document.createElement("h2");
+computerResults.textContent = "Computer: " + computerScore;
+let humanResults = document.createElement("h2");
+humanResults.textContent = "You: " + humanScore;
+let gameDesc = document.createElement("p");
+
+scoreDiv.appendChild(computerResults);
+scoreDiv.appendChild(humanResults);
+scoreDiv.appendChild(gameDesc);
+bodyref.appendChild(scoreDiv);
+
+function isWin(){
+    if(computerScore === 5){
+    gameDesc.textContent = "You lose! The robots have taken over!"
+    setButtonsDisabled(true);
+    humanScore = 0;
+    computerScore = 0;
+    createRestartButton();
+} else if(humanScore === 5){
+    gameDesc.textContent = "You win! You've saved us from the robot uprising!"
+    setButtonsDisabled(true);
+    humanScore = 0;
+    computerScore = 0;   
+    createRestartButton();
 }
+}
+
+function createRestartButton() {
+  const restartBtn = document.createElement("button");
+  restartBtn.textContent = "Play Again";
+  bodyref.appendChild(restartBtn);
+  score_button_container.appendChild(restartBtn)
+  restartBtn.addEventListener("click", () => {
+    humanScore = 0;
+    computerScore = 0;
+    computerResults.textContent = "Computer: " + computerScore;
+    humanResults.textContent = "You: " + humanScore;
+    gameDesc.textContent = "Rematch!";
+    restartBtn.remove();
+    setButtonsDisabled(false);
+  });
+}
+
 
 function playRound(humanChoice, computerChoice){
     if(humanChoice === 'rock' && computerChoice==='paper'){
-        console.log("You lose! Paper beats Rock");
-        computerScore+=1;
+        gameDesc.textContent="You lose! Paper beats Rock";
+        computerResults.textContent = `Computer:  ${computerScore+=1}`;
     } else if(humanChoice === 'rock' && computerChoice === 'scissors'){
-        console.log("You win! Rock beats scissors");
-        humanScore+=1;  
+        gameDesc.textContent="You win! Rock beats scissors";
+        humanResults.textContent = `You: ${humanScore+=1}`;  
     } else if(humanChoice === 'scissors' && computerChoice === 'rock'){
-        console.log("You lose! Rock beats scissors");
-        computerScore+=1;
+        gameDesc.textContent="You lose! Rock beats scissors";
+        computerResults.textContent = `Computer:  ${computerScore+=1}`;
     } else if(humanChoice === 'scissors' && computerChoice === 'paper'){
-        console.log("You win! Scissors beats paper");
-        humanScore+=1;
+        gameDesc.textContent="You win! Scissors beats paper";
+         humanResults.textContent = `You: ${humanScore+=1}`;
     } else if(humanChoice === 'paper' && computerChoice === 'rock'){
-        console.log("You win! Paper beats rock");
-        humanScore+=1;
+        gameDesc.textContent="You win! Paper beats rock";
+         humanResults.textContent = `You: ${humanScore+=1}`;
     } else if(humanChoice === 'paper' && computerChoice === 'scissors'){
-        console.log("You lose! Scissors beats paper");
-        computerScore+=1;
+        gameDesc.textContent="You lose! Scissors beats paper";
+        computerResults.textContent = `Computer:  ${computerScore+=1}`;
     }else{
-        console.log("You tie!")
-    }
-}
-
-function playGame(num = 5){
-    for(let i = 0; i < 5; i++){
-        if (num === 5 || num > 0){
-            playRound(getHumanChoice(), getComputerChoice());
-            num-=1;
-        }
+        gameDesc.textContent="You tie!"
     }
 }
 
 
 
+const rockBtn = document.createElement("button");
+const paperBtn = document.createElement("button");
+const scissorsBtn = document.createElement("button");
 
-playGame()
+const container = document.createElement("div")
+
+
+rockBtn.textContent = "Rock"
+paperBtn.textContent = "Paper"
+scissorsBtn.textContent = "Scissors"
+
+bodyref.appendChild(rockBtn)
+bodyref.appendChild(paperBtn)
+bodyref.appendChild(scissorsBtn)
+
+rockBtn.addEventListener("click", function(){
+    playRound("rock", getComputerChoice());
+    isWin();
+});
+paperBtn.addEventListener("click", function() {
+    playRound("paper", getComputerChoice());
+    isWin();
+});
+scissorsBtn.addEventListener("click", function(){
+    playRound("scissors", getComputerChoice());
+     isWin();
+})
+
+function setButtonsDisabled(state) {
+  rockBtn.disabled = state;
+  paperBtn.disabled = state;
+  scissorsBtn.disabled = state;
+}
+
+container.appendChild(rockBtn);
+container.appendChild(paperBtn);
+container.appendChild(scissorsBtn);
+
+score_button_container.appendChild(container)
+score_button_container.appendChild(scoreDiv)
+score_button_container.id = "score_btns"
+bodyref.appendChild(score_button_container)
